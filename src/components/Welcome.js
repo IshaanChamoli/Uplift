c906b8eea5cc4485c0047406bea20349bc07d5e0
HackOverflow404 <medhansh2005@gmail.com>
2024-02-24 16:43:15 -0600

    temp commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+
+export default function Welcome() {
+    return (
+        <View>
+            <h1>Welcome to</h1>
+            <h1>Uplift</h1>
+        </View>
+    );
+}
