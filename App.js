46fbef8edbe53d45750860aede26da4e0a32693a
seherb23 <97814597+seherb23@users.noreply.github.com>
2024-02-25 00:12:14 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AnswerSettings from './src/components/AnswerSettings';
+import Welcome from './src/components/Welcome';
 
 export default function App() {
   return (
 
     <View style={styles.container}>
-      <AnswerSettings />
+      <Welcome />
     </View>
   );
 }
